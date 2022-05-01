2240228e5ee2d9df126bae7240c5ce20e5c1bf3a
Magnus Brantheim <m.brantheim@gmail.com>
2022-05-01 22:40:44 +0200

    approve + depositaAll scripts

--- scripts/approve-vault.js
+++ scripts/approve-vault.js
@@ -1,10 +1,10 @@
 async function main() {
-  const vaultAddress = '0xC2cE269f3646a5F5bF1cCDa73c6cAB50f64012b6';
+  const vaultAddress = '0xDd957FbBdB549B957A1Db92b88bBA5297D0BbE99';
   const ERC20 = await ethers.getContractFactory('contracts/ERC20.sol:ERC20');
-  const fUSDTAddress = '0x049d68029688eabf473097a2fc38ef61633a3c7a';
-  const fUSDT = await ERC20.attach(fUSDTAddress);
-  await fUSDT.approve(vaultAddress, ethers.utils.parseEther('100'));
-  console.log('fUSDT approved');
+  const wantAddress = '0xB12BFcA5A55806AaF64E99521918A4bf0fC40802';
+  const want = await ERC20.attach(wantAddress);
+  await want.approve(vaultAddress, ethers.utils.parseEther('100'));
+  console.log('want approved');
 }
 
 main()
